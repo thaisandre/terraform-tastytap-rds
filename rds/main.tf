0ab76cd95229a343f62a689f21aecc0dd54598b9
thaisandre <thaisandre.sc@gmail.com>
2024-09-23 20:11:40 -0300

    refactor main.tf to use db variables

--- rds/main.tf
+++ rds/main.tf
@@ -74,9 +74,9 @@ resource "aws_db_instance" "default" {
   storage_type           = "gp2"
   engine                 = "mysql"
   engine_version         = "8.0"
-  db_name                = "tastytap"
-  username               = "db_username"
-  password               = "db_password"
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = var.db_password
   publicly_accessible = true
   skip_final_snapshot = true
   vpc_security_group_ids = [aws_security_group.tasytap-db-sg.id]
